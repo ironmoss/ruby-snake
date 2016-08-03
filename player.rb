4f80ffb6e59f4d9d2aeebfbeb1a962573579e4f8
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-08-03 11:13:18 -0700

    Player moves automatically
    The player now moves one space left by default.  No user input has been implemented yet.

--- player.rb
+++ player.rb
@@ -2,11 +2,15 @@ require_relative 'board'
 
 class Player
 
-  attr_reader :x_coordinate, :y_coordinate
+  attr_accessor :x_coordinate, :y_coordinate
 
   def initialize(board)
     @x_coordinate = board.width/2
     @y_coordinate = board.height/2
+  end
+
+  def move_player
+    @x_coordinate -= 1
   end
 
 end
