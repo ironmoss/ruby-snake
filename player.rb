f9420f0dfed0351b8e5d3120fb4995030b7f396d
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-08-03 11:32:18 -0700

    Movement is player parameter
    The player's movement speed (i.e., delay between refreshes) is now a player attribute set on instantiation.

--- player.rb
+++ player.rb
@@ -2,11 +2,12 @@ require_relative 'board'
 
 class Player
 
-  attr_accessor :x_coordinate, :y_coordinate
+  attr_accessor :x_coordinate, :y_coordinate, :speed
 
   def initialize(board)
     @x_coordinate = board.width/2
     @y_coordinate = board.height/2
+    @speed = 0.5
   end
 
   def move_player
