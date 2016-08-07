85951b3f7c276816597f30a644f6a40754360cfa
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-08-07 12:53:55 -0700

    Add Obstacle class
    Obstacles are randomly placed objects identical to bittles except that they kill the player on contact instead of causing it to grow.

---
+++ obstacle.rb
@@ -0,0 +1,10 @@
+class Obstacle
+
+  attr_reader :x_coordinate, :y_coordinate
+
+  def initialize(board)
+    @x_coordinate = rand(1..board.width - 2)
+    @y_coordinate = rand(0..board.height - 1)
+  end
+
+end
